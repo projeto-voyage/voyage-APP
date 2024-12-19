64f3b41e9957f2386950cdab7e24485c2f5afaab
afonso.araujo <afonso.araujo@supertroco.com.br>
2024-12-19 17:43:23 -0300

    primeiro commit do projeto fontend

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  name: 'App',
+  components: {
+    RouterView,
+  },
+}
+</script>
+
+<style scoped></style>
