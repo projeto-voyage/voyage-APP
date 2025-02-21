580e00d6e65b739043946162dd660756e61a0b34
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-02-20 22:05:42 -0300

    feat: nova página viewItinerary

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import ItineraryView from '../views/ItineraryView.vue'
+import viewItineraryView from '../views/viewItineraryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/itinerary',
       name: 'itinerary',
       component: ItineraryView,
+    },
+    {
+      path: '/itinerary/view',
+      name: 'itineraryview',
+      component: viewItineraryView,
     }
   ],
 })
