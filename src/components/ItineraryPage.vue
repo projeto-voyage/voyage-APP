94662135ea31bac32e7dad5a6036299ae0496944
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-03-03 23:53:33 -0300

    feat: interacao inicial com a api

--- src/components/ItineraryPage.vue
+++ src/components/ItineraryPage.vue
@@ -173,8 +173,8 @@
           this.isLoading = true;
           const response = await axios.post('http://localhost:3000/itineraries', {
             destination: this.inputData.destination,
-            budget: parseInt(this.inputData.budget),
-            duration: parseInt(this.inputData.duration)
+            totalDays: parseInt(this.inputData.duration),
+            totalCost: parseInt(this.inputData.budget)
           });
   
           this.successMessage = 'Roteiro gerado com sucesso!';
